85d20da7ee750506a4204fe9feca781d458545f4
Evgeniy <magiceg1@mail.ru>
2022-10-02 15:20:50 +0300

    Creating a symbols

--- little_snake/Program.cs
+++ little_snake/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hi, my little friend :D");
+﻿// Create a 3 points the symbols and enter the screen 
+int x1 = 5;
+int y1 = 5;
+char sym1 = '$';
+Draw(x1, y1, sym1);
+
+int x2 = 10;
+int y2 = 5;
+char sym2 = '*';
+Draw(x2, y2, sym2);
+
+int x3 = 15;
+int y3 = 5;
+char sym3 = '@';
+Draw(x3, y3, sym3);
+
+// Creating a method that will output characters 
+static void Draw(int x, int y, char sym)
+{
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine(sym);
+}
